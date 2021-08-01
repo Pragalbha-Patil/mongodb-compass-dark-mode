70795eb459a2c272cb0d88af0aeec8a4fa72e8f8
Pragalbha Patil <pragalbha.patil@gocomet.com>
2021-08-01 14:56:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,16 @@
 cd ~
 git clone https://github.com/Pragalbha-Patil/mongodb-compass-dark-mode.git
 cd mongodb-compass-dark-mode/
-npx asar pack ./ app.asar.new &
+tar -xf app.asar.tar.gz &
 BACK_PID=$!
 while kill -0 $BACK_PID ; do
-    echo "Building the new app.asar. Please wait..."
+    echo "Decompressing the new app.asar. Please wait..."
     sleep 10
 done
-sudo cp app.asar.new /usr/lib/mongodb-compass/resources/
-cd /usr/lib/mongodb-compass/resources/
-sudo mv app.asar app.asar.old
-sudo mv app.asar.new app.asar
+sudo mv /usr/lib/mongodb-compass/resources/app.asar app.asar.old
+sudo mv app.asar /usr/lib/mongodb-compass/resources/
 cd ~
+rm -rf mongodb-compass-dark-mode/
 echo "---------------------------------------------------------"
 echo "Done. Restart MongoDB Compass to see dark mode in effect."
 echo "---------------------------------------------------------"
-rm -rf mongodb-compass-dark-mode/
