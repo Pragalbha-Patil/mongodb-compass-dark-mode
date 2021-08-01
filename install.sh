77ad2f21a436d53c49b6a213eb285af57617ceca
Pragalbha Patil <pragalbha.patil@gocomet.com>
2021-08-01 14:47:46 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ git clone https://github.com/Pragalbha-Patil/mongodb-compass-dark-mode.git
 cd mongodb-compass-dark-mode/
 npx asar pack ./ app.asar.new &
 BACK_PID=$!
-wait $BACK_PID
+while kill -0 $BACK_PID ; do
+    echo "Building the new app.asar. Please wait..."
+    sleep 10
+done
 sudo cp app.asar.new /usr/lib/mongodb-compass/resources/
 cd /usr/lib/mongodb-compass/resources/
 sudo mv app.asar app.asar.old
